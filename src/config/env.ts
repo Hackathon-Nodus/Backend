a08bdb7f81d68b73c47510d7e3553fdd15d4bb1b
Emnet19 <emnbef456@gmail.com>
2026-04-17 18:41:38 +0300

    backend setup
    for the env.ts files the jwt secret key will be changed by the person who is working on the authentication part and also the mongoDB part will be changed by the person who is handling the database

---
+++ src/config/env.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+
+interface EnvConfig{
+    PORT: number,
+    NODE_ENV: string,
+    MONGO_URI: string,
+    JWT_SECRET:string,
+    JWT_REFRESH_TOKEN:string,
+    JWT_EXPIRES_IN:string,
+    JWT_REFRESH_EXPIRES_IN:string
+}
+
+export const env:EnvConfig={
+    PORT:parseInt(process.env.PORT || '3000',10),
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/myapp',
+    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
+    JWT_REFRESH_TOKEN: process.env.JWT_REFRESH_TOKEN || 'your_jwt_refresh_token',
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1h',
+    JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || '7d'
+}
